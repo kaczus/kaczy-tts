860809a749c9441b1bb3d571d77fffaa50cfba75
Marcin Kaczorek <marcin@karek.pl>
2024-09-04 21:28:13 +0200

    jak pobrać modele

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://huggingface.co/rhasspy/piper-voices
+git lfs pull en/en_GB/alan/medium/en_GB-alan-medium.onnx.json
+git lfs pull pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx.json
