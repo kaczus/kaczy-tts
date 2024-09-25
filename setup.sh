fe331e035444d5e842cc6008ef5840a62c7c823b
Marcin Kaczorek <marcin@karek.pl>
2024-09-25 14:27:03 +0200

    ściągaj wszystkie brytyjskie i polskie głosy

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 GIT_CLONE_PROTECTION_ACTIVE=false git clone https://huggingface.co/rhasspy/piper-voices
 pushd piper-voices/
-git lfs pull -I en/en_GB/alan/medium/
-git lfs pull -I pl/pl_PL/gosia/medium/
+git lfs pull -I en/en_GB/
+git lfs pull -I pl/pl_PL/
 popd
