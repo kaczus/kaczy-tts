71565ad317897df2f78f961a8cf497b1e3e27011
Marcin Kaczorek <marcin@karek.pl>
2024-09-25 18:29:00 +0200

    pobieraj głosy

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-GIT_CLONE_PROTECTION_ACTIVE=false git clone https://huggingface.co/rhasspy/piper-voices
-pushd piper-voices/
+deps="git-lfs lame piper"
+echo "Sprawdzam czy mamy wszystkie narzędzia:"
+for d in $deps; do
+	echo -n "$d: "
+	if $(which $d &> /dev/null); then
+		echo "OK"
+	else
+		echo "BRAK"
+		e=1
+	fi
+done
+[ -n "$e" ] && echo "Brakuje narzędzi, doinstaluj to co brakuje wyżej" && exit 1
+echo "Pobieram głosy"
+pushd piper-voices/ >/dev/null
 git lfs pull -I en/en_GB/
 git lfs pull -I pl/pl_PL/
-popd
+popd >/dev/null
