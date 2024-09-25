a9d8f3ed8625ca494e78e8fca9d04632c522fe16
Marcin Kaczorek <marcin@karek.pl>
2024-09-25 14:13:53 +0200

    pobieraj z lfs w skrypcie

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-git clone https://huggingface.co/rhasspy/piper-voices
-git lfs pull en/en_GB/alan/medium/en_GB-alan-medium.onnx.json
-git lfs pull pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx.json
+GIT_CLONE_PROTECTION_ACTIVE=false git clone https://huggingface.co/rhasspy/piper-voices
+pushd piper-voices/
+git lfs pull -I en/en_GB/alan/medium/
+git lfs pull -I pl/pl_PL/gosia/medium/
+popd
