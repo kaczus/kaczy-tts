db4586e5b88634cc12dc414dc629c062bc39e963
Marcin Kaczorek <marcin@karek.pl>
2024-09-25 13:19:24 +0200

    czytaj ze stdin

--- gadaj.sh
+++ gadaj.sh
@@ -1,1 +1,5 @@
-onnx=./piper-voices/pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx; json=./piper-voices/pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx.json; echo "co teraz?" | piper -m $onnx -c $json --output_file - | aplay
+onnx=$PWD/piper-voices/pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx
+json=$PWD/piper-voices/pl/pl_PL/gosia/medium/pl_PL-gosia-medium.onnx.json
+while read in; do
+	echo $in | piper -m $onnx -c $json --output_file - | aplay
+done
